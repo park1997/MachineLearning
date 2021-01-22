5675432b97404fc2fa3aeab4e4a6e14cf487cb3f
park1997 <iambyunghyun97@gmail.com>
2021-01-22 17:45:27 +0900

    Create 2-5 Stratified K 폴드.py

---
+++ 2장/2-5 Stratified K 폴드.py
@@ -0,0 +1,13 @@
+import pasdas as pd
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import KFold
+from sklearn.tree import DecisionTreeClassifier
+
+
+
+
+
+
+
